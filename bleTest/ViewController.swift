29117f2ac679e9647cc7ced42aa70860705996bb
EriaWist <c121680118@gmail.com>
2022-02-14 22:51:13 +0800

    Update ViewController.swift

--- bleTest/ViewController.swift
+++ bleTest/ViewController.swift
@@ -19,13 +19,15 @@ class ViewController: UIViewController,ObservableObject {
     func setUP(){
         centralManager = CBCentralManager(delegate: self, queue: nil) //
     }
-    func starScan(){
+    func starScan(){ //不要直接啟動，在 centralManagerDidUpdateState 確認 central.state 為 .poweredOn 時呼叫
         centralManager.scanForPeripherals(withServices: nil, options: [CBCentralManagerScanOptionAllowDuplicatesKey:true]) //
     }
     func stop(){
-        self.centralManager.stopScan() //
+        self.centralManager.stopScan() //停止掃描、當 centralManagerDidUpdateState 的 central.state 為 .poweredOff
     }
-    
+    func connect(peripheral: CBPeripheral){
+        centralManager.connect(peripheral, options: nil) //nil連接不指定 UUID
+    }
 }
 extension ViewController:CBCentralManagerDelegate{
     //peripheral
@@ -51,18 +53,33 @@ extension ViewController:CBCentralManagerDelegate{
         }
         
     }
-     func centralManager(_ central: CBCentralManager,
-                         didDiscover peripheral: CBPeripheral,
-                   advertisementData: [String : Any],
-                                rssi RSSI: NSNumber) {
-         //當偵測到東西
+    func centralManager(_ central: CBCentralManager,
+                        didDiscover peripheral: CBPeripheral,
+                        advertisementData: [String : Any],
+                        rssi RSSI: NSNumber) {
+        //當偵測到東西
         if(peripheral.name == nil || peripheral.name?.count == 0)
         {
             return
         }
-//        print(peripheral.name as! String)
+        //        print(peripheral.name as! String)
         peripheralsArr[peripheral.name!] = peripheral
     }
+    func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
+        //連接成功時呼叫
+        peripheral.delegate = self
+        peripheral.discoverServices(nil)
+    }
+    func centralManager(_ central: CBCentralManager, didFailToConnect peripheral: CBPeripheral, error: Error?) {
+        print("連接失敗")
+        print(error)
+    }
     
-    
+}
+
+extension ViewController:CBPeripheralDelegate{
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
+        //連接的裝置有發好 Services
+        print(peripheral.services)
+    }
 }
